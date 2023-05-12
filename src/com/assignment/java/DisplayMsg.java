089628c3ce25da5a699c6645278856a0aaad59dd
Prashant1424 <prashant1424325@gmail.com>
2023-05-13 02:29:36 +0530

    aster

--- src/com/assignment/java/DisplayMsg.java
+++ src/com/assignment/java/DisplayMsg.java
@@ -2,6 +2,7 @@ package com.assignment.java;
 
 public class DisplayMsg {
 
+//	Write a program to display a message
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World! I'm back...");
