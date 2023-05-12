496d9188e6064c2aa776fef50be1adac4e1ab037
Prashant1424 <prashant1424325@gmail.com>
2023-05-13 03:23:01 +0530

    Adding : Second Assignment i.e. Displaying Default Value of Data Types of Java

---
+++ src/com/assignment/java/DisplayPrimitiveDataTypes.java
@@ -0,0 +1,31 @@
+package com.assignment.java;
+
+public class DisplayPrimitiveDataTypes {
+	
+//	Write a java program to display the default value of all Primitive Data types of Java
+	
+	static byte a;
+	static short b;
+	static int c;
+	static long d;
+	static float e;
+	static double f;
+	static char g;
+	static boolean h;
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("The Default value of Byte Data type is :" + a);
+		System.out.println("The Default value of Short Data type is :" + b);
+		System.out.println("The Default value of Integer Data type is :" + c);
+		System.out.println("The Default value of Long Data type is :" + d);
+		System.out.println("The Default value of Float Data type is :" + e);
+		System.out.println("The Default value of Double Data type is :" + f);
+		System.out.println("The Default value of Char Data type is :" + g);
+		System.out.println("The Default value of Boolean Data type is :" + h);
+		
+
+	}
+
+}
